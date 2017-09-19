cc0cbfaa2569892a686b3847ab5249d7dfc1cec6
Westery <xishanxiaoke@foxmail.com>
2017-09-19 21:39:03 +0800

    更新config路径指向

--- src/SmsServiceProvider.php
+++ src/SmsServiceProvider.php
@@ -18,7 +18,7 @@ class SmsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'./config/sms.php' => config_path('sms.php'),
+            __DIR__.'/config/sms.php' => config_path('sms.php'),
         ]);
     }
 
@@ -30,7 +30,7 @@ class SmsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'./config/sms.php', 'sms'
+            __DIR__.'/config/sms.php', 'sms'
         );
         $this->app->singleton('Sms', function ($app) {
             $app = new Sms(config('sms.default'));
