b7d2671c3d0e5e7fde962d53e5b4f344717f9838
Westery <xishanxiaoke@foxmail.com>
2017-09-19 22:27:36 +0800

    新增发送验证码短信

--- src/lib/NetEase.php
+++ src/lib/NetEase.php
@@ -158,10 +158,10 @@ class NetEase{
 
     /**
      * 发送模板短信
-     * @param  $templateid       [模板编号(由客服配置之后告知开发者)]
-     * @param  $mobiles          [验证码]
-     * @param  $params          [短信参数列表，用于依次填充模板，JSONArray格式，如["xxx","yyy"];对于不包含变量的模板，不填此参数表示模板即短信全文内容]
-     * @return $result      [返回array数组对象]
+     * @param $templateid [模板编号(由客服配置之后告知开发者)]
+     * @param array $mobiles [手机号]
+     * @param string $params [短信参数列表，用于依次填充模板，JSONArray格式，如["xxx","yyy"];对于不包含变量的模板，不填此参数表示模板即短信全文内容]
+     * @return array
      */
     public function sendSMSTemplate($templateid,$mobiles=array(),$params=''){
         $url = 'https://api.netease.im/sms/sendtemplate.action';
@@ -176,6 +176,24 @@ class NetEase{
             $result = $this->postDataFsockopen($url,$data);
         }
         return $result;
+    }
+
+    /**
+     * 发送短信验证码
+     * @param $mobile  [手机号]
+     * @param $code   [验证码]
+     * @return array
+     */
+    protected function sendSmsCode($mobile,$code){
+        $url = 'https://api.netease.im/sms/sendcode.action';
+        $data = array('mobile'=>$mobile,'authCode'=>$code);
+        if($this->RequestType=='curl'){
+            $result = $this->postDataCurl($url,$data);
+        }else{
+            $result = $this->postDataFsockopen($url,$data);
+        }
+        return $result;
+
     }
 }
 
